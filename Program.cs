e9ef0948221d8fb6226bea9ef5cc0bcf8e57dc4c
Shaary <kallisto3261@outlook.com>
2020-05-07 19:49:28 -0600

    Initial commit.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyFirstApp.Core;
+using MyFirstApp.Data;
+using System;
 
 namespace MyFirstApp
 {
@@ -7,6 +9,21 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter schema name: ");
+            var schema = Console.ReadLine();
+            Console.WriteLine("Enter stored procedure name: ");
+            var procName = Console.ReadLine();
+
+            var repository = new Repository(schema, procName);
+
+            var parametersNames = repository.ParametersNamesGet();
+            var parametersData = repository.ParametersDataGet(parametersNames);
+
+            var query = new StoredProcQuery(procName, parametersNames, parametersData);
+
+            Console.WriteLine(query);
         }
+
     }
 }
