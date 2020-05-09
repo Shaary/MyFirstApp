7f11cb38abb8ba877a4b2131046c57c8447be65b
Shaary <kallisto3261@outlook.com>
2020-05-08 23:20:00 -0600

    Can pull parameter's names, get table and column names out of them.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
-﻿using MyFirstApp.Core;
+﻿using Microsoft.Data.SqlClient;
+using MyFirstApp.Core;
 using MyFirstApp.Data;
 using System;
+using System.Data;
+using Dapper;
+using System.Linq;
 
 namespace MyFirstApp
 {
@@ -8,21 +12,40 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Enter schema name: ");
-            var schema = Console.ReadLine();
-            Console.WriteLine("Enter stored procedure name: ");
-            var procName = Console.ReadLine();
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=AmazingDb; Integrated Security=True;";
 
-            var repository = new Repository(schema, procName);
+            //Console.WriteLine("Enter schema name: ");
+            //var schema = Console.ReadLine();
+            //Console.WriteLine("Enter stored procedure name: ");
+            //var procName = Console.ReadLine();
 
-            var parametersNames = repository.ParametersNamesGet();
-            var parametersData = repository.ParametersDataGet(parametersNames);
+            var repository = new Repository(connectionString, "fs", "usp_AccessTypesGet");
 
-            var query = new StoredProcQuery(procName, parametersNames, parametersData);
 
-            Console.WriteLine(query);
+
+            var parameters = repository.ParametersNamesGet();
+
+            //foreach (string param in parameters)
+            //{
+            //    Console.WriteLine(param);
+            //}
+
+            repository.ParametersDataGet(parameters);
+
+
+
+
+            //Test methods
+            //IDbConnection db = new SqlConnection(connectionString);
+            //var data = db.Query("select * from fs.AccessTypes").ToList();
+
+            //var parametersNames = repository.ParametersNamesGet();
+            //var parametersData = repository.ParametersDataGet(parametersNames);
+
+            //var query = new StoredProcQuery(procName, parametersNames, parametersData);
+
+            //Console.WriteLine(query);
         }
 
     }
